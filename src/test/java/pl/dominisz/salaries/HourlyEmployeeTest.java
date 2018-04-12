24e29ac529920eb3856d69437a6abde1671dfc1b
dominisz <dominisz@gmail.com>
2018-04-12 19:31:10 +0200

    HourlyEmployeeTest

--- src/test/java/pl/dominisz/salaries/HourlyEmployeeTest.java
+++ src/test/java/pl/dominisz/salaries/HourlyEmployeeTest.java
@@ -46,4 +46,28 @@ public class HourlyEmployeeTest {
         hourlyEmployee.addWorkingDay(workingDay);
         assertEquals(new BigDecimal(8), hourlyEmployee.getSalary(LocalDate.of(2018, 4, 13)));
     }
+
+    @Test
+    void shouldReturnOverhourSalaryForOneDay() {
+        HourlyEmployee hourlyEmployee = new HourlyEmployee("name", BigDecimal.ONE);
+        WorkingDay workingDay = new WorkingDay(LocalDate.of(2018, 4, 12), 12);
+        hourlyEmployee.addWorkingDay(workingDay);
+        BigDecimal expectedSalary = new BigDecimal(14);
+        BigDecimal actualSalary = hourlyEmployee.getSalary(LocalDate.of(2018, 4, 13));
+        assertTrue(expectedSalary.compareTo(actualSalary) == 0);
+    }
+
+    @Test
+    void shouldReturnOverhourSalaryForOneDayWithoutOtherDays() {
+        HourlyEmployee hourlyEmployee = new HourlyEmployee("name", BigDecimal.ONE);
+        WorkingDay workingDay = new WorkingDay(LocalDate.of(2018, 4, 12), 12);
+        WorkingDay earlierDay = new WorkingDay(LocalDate.of(2018, 4, 1), 8);
+        WorkingDay laterDay = new WorkingDay(LocalDate.of(2018, 4, 15), 8);
+        hourlyEmployee.addWorkingDay(workingDay);
+        hourlyEmployee.addWorkingDay(earlierDay);
+        hourlyEmployee.addWorkingDay(laterDay);
+        BigDecimal expectedSalary = new BigDecimal(14);
+        BigDecimal actualSalary = hourlyEmployee.getSalary(LocalDate.of(2018, 4, 13));
+        assertTrue(expectedSalary.compareTo(actualSalary) == 0);
+    }
 }
