caea8d793cc738be1e6d33ff6fbca43eace09dba
dominisz <dominisz@gmail.com>
2018-04-12 17:42:25 +0200

    HourlyEmployee

--- src/main/java/pl/dominisz/salaries/Employee.java
+++ src/main/java/pl/dominisz/salaries/Employee.java
@@ -1,12 +1,20 @@
 package pl.dominisz.salaries;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 /**
  * http://dominisz.pl
  * 11.04.2018
  */
 public abstract class Employee {
 
-    //fullname
-    //getSalary(date)
-    //getFirstWorkingDay()
+    private String name;
+
+    public abstract BigDecimal getSalary(LocalDate date);
+
+    public abstract LocalDate getFirstDayOfWorkingPeriod(LocalDate date);
+
+    public abstract boolean isPayDay(LocalDate date);
+
 }
--- src/main/java/pl/dominisz/salaries/HourlyEmployee.java
+++ src/main/java/pl/dominisz/salaries/HourlyEmployee.java
@@ -1,10 +1,34 @@
 package pl.dominisz.salaries;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 /**
  * http://dominisz.pl
  * 11.04.2018
  */
 public class HourlyEmployee extends Employee {
 
-    //hourlyRate
+    private BigDecimal hourlyRate;
+
+
+    @Override
+    public BigDecimal getSalary(LocalDate date) {
+        if (isPayDay(date)) {
+            return BigDecimal.ZERO;
+        } else {
+            return BigDecimal.ZERO;
+        }
+    }
+
+    @Override
+    public LocalDate getFirstDayOfWorkingPeriod(LocalDate date) {
+        return null;
+    }
+
+    @Override
+    public boolean isPayDay(LocalDate date) {
+        return false;
+    }
+
 }
